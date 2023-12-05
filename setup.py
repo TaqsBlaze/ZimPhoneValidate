e069202a22f571c32000281344bef2b696d5fe2d
Tanaka Chinengundu <33166177+TaqsBlaze@users.noreply.github.com>
2023-12-05 11:24:04 +0200

    Update setup.py
    Fixed long_description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0.0",
     author="Tanaka Chinengundu",
     author_email="tanakah30@gmail.com",
-    description="A package for easily validating Zimbabwean phone numbers in your python applications",
+    long_description="A package for easily validating Zimbabwean phone numbers in your python applications",
     license="MIT",
     url="https://wa.me/263778040497?text=More+details+about+ZimPhoneValidate+package",
     install_requires=["pip"],
